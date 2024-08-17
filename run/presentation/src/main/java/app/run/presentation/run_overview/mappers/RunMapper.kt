5ad3bf64669e908e44b1ba498ad674fd39557859
MorPfa <moritzpfatteicher@gmail.com>
2024-08-16 19:25:34 -0700

    added run overview stats

---
+++ run/presentation/src/main/java/app/run/presentation/run_overview/mappers/RunMapper.kt
@@ -0,0 +1,30 @@
+package app.run.presentation.run_overview.mappers
+
+import app.core.domain.run.Run
+import app.core.presentation.ui.formatted
+import app.core.presentation.ui.toFormattedKm
+import app.core.presentation.ui.toFormattedKmh
+import app.core.presentation.ui.toFormattedMeters
+import app.core.presentation.ui.toFormattedPace
+import app.run.presentation.run_overview.model.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc.withZoneSameInstant(ZoneId.systemDefault())
+    val formattedDateTime =
+        DateTimeFormatter.ofPattern("MMM dd, yyyy - hh:mma").format(dateTimeInLocalTime)
+    val distanceKm = distanceMeters / 1000.0
+    return RunUi(
+        id = id!!,
+        duration = duration.formatted(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmh.toFormattedKmh(),
+        maxSpeed = maxSpeedKmh.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+
+    )
+}
